5ca9d1b10299509aeeab6084baa11f966e887222
Abderrazzak Asmaayni <zak@MacBooks-MacBook-Pro.local>
2020-03-26 14:32:24 -0400

    committing

---
+++ frontend/src/app/categories.ts
@@ -0,0 +1,7 @@
+export const categories = [
+    "Electronics",
+    "Home And Gardens",
+    "Fashion",
+    "Toyes"
+  ];
+  
---
+++ frontend/src/app/left-nav/left-nav.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { categories } from '../categories';
+
+@Component({
+  selector: 'app-left-nav',
+  templateUrl: './left-nav.component.html',
+  styleUrls: ['./left-nav.component.css']
+})
+export class LeftNavComponent implements OnInit {
+    categories = categories;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
